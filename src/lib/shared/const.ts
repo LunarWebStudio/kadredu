c7cd6a5becc15245efe8fd65ce5075153610c19e
Denis Smirnov <w1png@yandex.ru>
2024-05-28 13:53:11 +0300

    Add updateSelf

---
+++ src/lib/shared/const.ts
@@ -0,0 +1,5 @@
+// profile
+
+export const NAME_LIMIT = 255;
+export const DESCRIPTION_LIMIT = 1000;
+export const MAX_PROFILE_PICTURE_SIZE = 5 * 1024 * 1024;
--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,5 +1,59 @@
-// import { z } from "zod";
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+import { UploadFile } from "~/lib/server/file_upload";
+import { ProcessImage } from "~/lib/server/images";
 
-// import { createTRPCRouter } from "~/server/api/trpc";
+import { createTRPCRouter, onboardingProcedure } from "~/server/api/trpc";
+import { images, users } from "~/server/db/schema";
+import { DESCRIPTION_LIMIT, MAX_PROFILE_PICTURE_SIZE, NAME_LIMIT } from "~/lib/shared/const";
 
-// export const userRouter = createTRPCRouter({});
+export const userRouter = createTRPCRouter({
+  updadeSelf: onboardingProcedure
+    .input(
+      z.object({
+        name: z
+          .string({
+            required_error: "ФИО не заполнено",
+            invalid_type_error: "ФИО не является строкой"
+          })
+          .min(1, "ФИО не заполнено")
+          .max(NAME_LIMIT)
+          .optional(),
+        description: z
+          .string({
+            required_error: "Описание не заполнено",
+            invalid_type_error: "Описание не является строкой"
+          })
+          .max(DESCRIPTION_LIMIT)
+          .optional(),
+        profilePictureImage: z
+          .string({
+            required_error: "Фото не выбрано",
+            invalid_type_error: "Фото не является строкой"
+          })
+          .max(MAX_PROFILE_PICTURE_SIZE, "Фото слишком большое")
+          .optional()
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.transaction(async (tx) => {
+        let imageId = "";
+        if (input.profilePictureImage) {
+          const processed_image = await ProcessImage(input.profilePictureImage);
+          const storageId = `profile_picture_${ctx.session.user.id}`
+          await UploadFile(processed_image.file, storageId);
+
+          imageId = (await tx.insert(images).values({
+            blurPreview: processed_image.blurPreview,
+            storageId
+          }).returning())[0]!.id;
+        }
+
+        await tx.update(users).set({
+          name: input.name,
+          description: input.description,
+          profilePictureId: imageId
+        }).where(eq(users.id, ctx.session.user.id));
+      })
+    })
+});
