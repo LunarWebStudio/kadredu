c470152e8a9c5096468e6c56c33321e98437fd90
Denis Smirnov <w1png@yandex.ru>
2024-06-21 12:34:19 +0300

    Fix sidebar resize

--- src/app/(website)/profile/layout.tsx
+++ src/app/(website)/profile/layout.tsx
@@ -118,7 +118,7 @@ export default async function ProfileLayout({
           </div>
         </div>
       </aside>
-      {children}
+      <div className="shrink grow">{children}</div>
     </div>
   );
 }
