3bef55e54096c0e06366d1f0c0e7eb930598fa24
bou1der <krowkin@bk.ru>
2024-10-06 21:09:41 +0300

    Corrections

--- src/lib/server/redis.ts
+++ src/lib/server/redis.ts
@@ -1,38 +1,6 @@
 import { createClient } from "redis"
 import { env } from "~/env"
 
-
-// class Redis {
-//   private readonly client = createClient({
-//     url: env.REDIS_URL
-//   })
-//
-//   constructor(){
-//     this.client.connect()
-//   }
-//
-//   async createAchievement(achievement: Achievement){
-//     await this.client.HSET(
-//       `achievements:${achievement.eventType}`,
-//       [
-//         achievement.id, achievement.eventAmount,
-//       ])
-//   } 
-//
-//   async countEvent(id:string, event: EventType, cb: (achievementId:string) => void){
-//     const count = await this.client.incrBy(`user:${id}:${event}`, 1)
-//
-//     const hash = await this.client.hGetAll(`achievements:${event}`)
-//     for (const key in hash){
-//       if (Number(hash[key]) === count) cb(key)
-//     }
-//   }
-//
-//   async deleteAchievement(id:string, event: EventType){
-//     await this.client.HDEL(`achievement:${event}`, id)
-//   }
-// }
-
 const redis = createClient({
   url: env.REDIS_URL
 })
